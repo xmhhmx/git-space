194ce907bf869084836c6de1ce6194041a6509dd
mX1@0 <mX1@0@xy.com>
2025-07-07 19:51:43 +0800

    second commit

--- src/main/java/org/example/Gittest.java
+++ src/main/java/org/example/Gittest.java
@@ -3,5 +3,6 @@ package org.example;
 public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello hello ");
+        System.out.println("hello hello 111");
     }
 }
