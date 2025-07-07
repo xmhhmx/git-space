6930e83bd4ea6a4428ff24b32882fd10fedbab82
mX1@0 <mX1@0@xy.com>
2025-07-07 19:55:08 +0800

    third commit

--- src/main/java/org/example/Gittest.java
+++ src/main/java/org/example/Gittest.java
@@ -4,5 +4,6 @@ public class Gittest {
     public static void main(String[] args) {
         System.out.println("hello hello ");
         System.out.println("hello hello 111");
+        System.out.println("hello hello 222222");
     }
 }
